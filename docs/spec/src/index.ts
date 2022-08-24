70613d55f94a953d185d32b2c1763aeda7339d47
Leiko Ravelo <ravelo.leiko@gmail.com>
2022-08-24 10:01:07 +0800

    Refactor VPN Gateway and unit test, add VPC -HAS-> VPN Gateway relationship

--- docs/spec/src/index.ts
+++ docs/spec/src/index.ts
@@ -3,16 +3,9 @@ import { IntegrationSpecConfig } from '@jupiterone/integration-sdk-core';
 import { IntegrationConfig } from '../../../src/config';
 import { albSpec } from './alb';
 import { ecsSpec } from './ecs';
-import { vpnSpec } from './vpn';
 import { vpcSpec } from './vpc';
 import { autoScalingSpec } from './auto-scaling';
 
 export const invocationConfig: IntegrationSpecConfig<IntegrationConfig> = {
-  integrationSteps: [
-    ...ecsSpec,
-    ...albSpec,
-    ...vpnSpec,
-    ...vpcSpec,
-    ...autoScalingSpec,
-  ],
+  integrationSteps: [...ecsSpec, ...albSpec, ...vpcSpec, ...autoScalingSpec],
 };
--- src/steps/index.ts
+++ src/steps/index.ts
@@ -1,13 +1,11 @@
 import { albSteps } from './alb/index';
 import { ecsSteps } from './ecs/index';
-import { vpnSteps } from './vpn';
 import { vpcSteps } from './vpc';
 import { autoScalingSteps } from './auto-scaling';
 
 const integrationSteps = [
   ...ecsSteps,
   ...albSteps,
-  ...vpnSteps,
   ...vpcSteps,
   ...autoScalingSteps,
 ];
